24e3c08acb410c95365686e956e6ed6facc27ff1
jhon082010 <158795575+jhon082010@users.noreply.github.com>
2024-04-17 08:08:13 +0800

    Update SillyStringUtils.sol

--- SillyStringUtils.sol
+++ SillyStringUtils.sol
@@ -4,13 +4,13 @@ pragma solidity ^0.8.17;
 
 library SillyStringUtils {
 
-struct Haiku {
-string line1;
-string line2;
-string line3;
-}
+    struct Haiku {
+        string line1;
+        string line2;
+        string line3;
+    }
 
-function shruggie(string memory _input) internal pure returns (string memory) {
-return string.concat(_input, unicode" 🤷 ");
-}
+    function shruggie(string memory _input) internal pure returns (string memory) {
+        return string.concat(_input, unicode" 🤷");
+    }
 }
